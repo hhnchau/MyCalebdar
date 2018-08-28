f8e85609daba789baacd5974b7385798e090df37
kingpes <kingpes#gmail.com>
2018-08-28 14:53:14 +0700

    Commit

---
+++ app/src/main/java/com/example/chauhn/mycalendar/interfaces/OnDateSelectedListener.java
@@ -0,0 +1,13 @@
+package com.example.chauhn.mycalendar.interfaces;
+
+
+import com.example.chauhn.mycalendar.objects.CalendarDate;
+
+/**
+ * Created by ShirlyKadosh on 4/20/17.
+ */
+
+public interface OnDateSelectedListener {
+
+    void onDateSelected(CalendarDate date);
+}
---
+++ app/src/main/java/com/example/chauhn/mycalendar/interfaces/OnDayViewClickListener.java
@@ -0,0 +1,13 @@
+package com.example.chauhn.mycalendar.interfaces;
+
+
+import com.example.chauhn.mycalendar.views.CalendarDayView;
+
+/**
+ * Created by ShirlyKadosh on 4/20/17.
+ */
+
+public interface OnDayViewClickListener {
+
+    void onDayViewClick(CalendarDayView view);
+}
